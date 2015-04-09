0546d3364554b9365f2bffc4b4ddd709706c0304
Yancy Knight <yancy.knight@gmail.com>
2015-04-09 13:57:56 -0600

    GUI now has 20 x 20 buttons.

--- GUI.java
+++ GUI.java
@@ -1,5 +1,4 @@
-import java.awt.Container;
-import java.awt.GridLayout;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
@@ -11,6 +10,18 @@ public class GUI extends JFrame implements ActionListener{
     static Board b;
     private ArrayList<ArrayList<JButton>> buttons = new ArrayList<ArrayList<JButton>>();
     Icon bn = new ImageIcon(getClass().getResource("bn.png"));
+    Icon bb = new ImageIcon(getClass().getResource("bb.png"));
+    Icon bf = new ImageIcon(getClass().getResource("bf.png"));
+    Icon b0 = new ImageIcon(getClass().getResource("b0.png"));
+    Icon b1 = new ImageIcon(getClass().getResource("b1.png"));
+    Icon b2 = new ImageIcon(getClass().getResource("b2.png"));
+    Icon b3 = new ImageIcon(getClass().getResource("b3.png"));
+    Icon b4 = new ImageIcon(getClass().getResource("b4.png"));
+    Icon b5 = new ImageIcon(getClass().getResource("b5.png"));
+    Icon b6 = new ImageIcon(getClass().getResource("b6.png"));
+    Icon b7 = new ImageIcon(getClass().getResource("b7.png"));
+    Icon b8 = new ImageIcon(getClass().getResource("b8.png"));
+
 
     public static void main(String args[]){
         GUI gui = new GUI();
@@ -40,7 +51,8 @@ public class GUI extends JFrame implements ActionListener{
                 button.setIcon(bn);
                 int num = b.getTile(j,i);
 
-                button.setActionCommand(Integer.toString(j) + Integer.toString(i) + Integer.toString(num) );
+                button.setActionCommand(Integer.toString(j) + Integer.toString(i) + Integer.toString(num));
+                button.setPreferredSize(new Dimension(22,22));
                 button.addActionListener(this);
 
                 pane.add(button);
@@ -68,11 +80,11 @@ public class GUI extends JFrame implements ActionListener{
 
         switch(state) {
             case -1:
-                Icon bb = new ImageIcon(getClass().getResource("bb.png"));
                 buttons.get(x).get(y).setIcon(bb);
+                JOptionPane.showMessageDialog(null, "You done got dead", "BOOM", JOptionPane.INFORMATION_MESSAGE);
+
                 break;
             case 0:
-                Icon b0 = new ImageIcon(getClass().getResource("b0.png"));
                 buttons.get(x).get(y).setIcon(b0);
                 if(x - 1 >= 0 && buttons.get(x - 1).get(y).getIcon() == bn) {
                     revealTile(x - 1, y);
@@ -88,35 +100,27 @@ public class GUI extends JFrame implements ActionListener{
                 }
                 break;
             case 1:
-                Icon b1 = new ImageIcon(getClass().getResource("b1.png"));
                 buttons.get(x).get(y).setIcon(b1);
                 break;
             case 2:
-                Icon b2 = new ImageIcon(getClass().getResource("b2.png"));
                 buttons.get(x).get(y).setIcon(b2);
                 break;
             case 3:
-                Icon b3 = new ImageIcon(getClass().getResource("b3.png"));
                 buttons.get(x).get(y).setIcon(b3);
                 break;
             case 4:
-                Icon b4 = new ImageIcon(getClass().getResource("b4.png"));
                 buttons.get(x).get(y).setIcon(b4);
                 break;
             case 5:
-                Icon b5 = new ImageIcon(getClass().getResource("b5.png"));
                 buttons.get(x).get(y).setIcon(b5);
                 break;
             case 6:
-                Icon b6 = new ImageIcon(getClass().getResource("b6.png"));
                 buttons.get(x).get(y).setIcon(b6);
                 break;
             case 7:
-                Icon b7 = new ImageIcon(getClass().getResource("b7.png"));
                 buttons.get(x).get(y).setIcon(b7);
                 break;
             case 8:
-                Icon b8 = new ImageIcon(getClass().getResource("b8.png"));
                 buttons.get(x).get(y).setIcon(b8);
                 break;
         }
